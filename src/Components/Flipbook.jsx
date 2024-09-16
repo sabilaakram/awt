f01eb3f2e2f97d1919a99b63db59ab31a8524361
Hamza Ejaz <hejaz389@gmail.com>
2024-09-16 12:10:22 +0500

    updated flipbook

--- src/Components/Flipbook.jsx
+++ src/Components/Flipbook.jsx
@@ -1,80 +1,33 @@
-import React, { useState, useEffect, useRef } from "react";
-import { Document, Page, pdfjs } from "react-pdf";
+import React from "react";
 import { Container } from "react-bootstrap";
-import HTMLFlipBook from "react-pageflip";
-import pdfURL from "../assets/AWT-Corporate-Profile.pdf";
-
-pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
 
 const PDFFlipbook = () => {
-  const [numPages, setNumPages] = useState(null);
-  const [pageWidth, setPageWidth] = useState(0);
-
-  const bookRef = useRef(null);
-
-  useEffect(() => {
-    const updateDimensions = () => {
-      const width = window.innerWidth;
-      setPageWidth(Math.min(width * 0.9, 1000));
-    };
-
-    window.addEventListener("resize", updateDimensions);
-    updateDimensions();
-
-    // Add keyboard event listener
-    const handleKeyDown = (event) => {
-      if (event.key === "ArrowRight") {
-        bookRef.current.pageFlip().flipNext();
-      } else if (event.key === "ArrowLeft") {
-        bookRef.current.pageFlip().flipPrev();
-      }
-    };
-
-    window.addEventListener("keydown", handleKeyDown);
-
-    // Cleanup
-    return () => {
-      window.removeEventListener("resize", updateDimensions);
-      window.removeEventListener("keydown", handleKeyDown);
-    };
-  }, []);
-
-  const onDocumentLoadSuccess = ({ numPages }) => {
-    setNumPages(numPages);
-  };
-
   return (
     <Container>
-      <Document
-        file={pdfURL}
-        onLoadSuccess={onDocumentLoadSuccess}
-        error="An error occurred while loading the PDF."
+      <div
+        style={{
+          position: "relative",
+          paddingTop: "max(60%, 324px)",
+          width: "100%",
+          height: "0",
+        }}
       >
-        {numPages && (
-          <HTMLFlipBook
-            width={pageWidth}
-            height={750}
-            maxShadowOpacity={0.5}
-            showCover={true}
-            mobileScrollSupport={true}
-            ref={bookRef}
-          >
-            {Array.from(new Array(numPages), (el, index) => (
-              <div key={index} className="demoPage">
-                <Page
-                  pageNumber={index + 1}
-                  width={550}
-                  renderTextLayer={false}
-                  renderAnnotationLayer={false}
-                  scale={2}
-                  renderMode="svg"
-                  devicePixelRatio={2}
-                />
-              </div>
-            ))}
-          </HTMLFlipBook>
-        )}
-      </Document>
+        <iframe
+          style={{
+            position: "absolute",
+            border: "none",
+            width: "100%",
+            height: "100%",
+            left: 0,
+            top: 0,
+          }}
+          title="flipbook"
+          src="https://online.fliphtml5.com/gzija/dqtx/"
+          seamless="seamless"
+          allowTransparency="true"
+          allowFullScreen="true"
+        ></iframe>
+      </div>
     </Container>
   );
 };
